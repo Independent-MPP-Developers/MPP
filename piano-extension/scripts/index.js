4ebc1898d3ad4dd12a8dae4877cdf0adbf1116d7
Hustoroff <84928386+Hustoroff@users.noreply.github.com>
2021-10-17 16:28:32 +0300

    index.js

--- piano-extension/scripts/index.js
+++ piano-extension/scripts/index.js
@@ -1,1 +1,4 @@
-console.log('Hello World!');
+console.log("%c MPP|Extension ", "background: #222; color: #bada55; font-size: 40px");
+console.log("%c Extension for Multiplayer Piano is designed to modify the client using scripts that are downloaded by other people. ", "background: #222; color: #bada55; font-size: 20px")
+console.log("%c If you have any problems with the extension, please let us know: Hustandant#8787 or Habib#7718 ", "background: #222; color: #bada55; font-size: 20px");
+
