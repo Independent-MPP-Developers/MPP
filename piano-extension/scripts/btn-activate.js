7cf54fa6abca63347c7bb8430a315fd6191ee940
Hustoroff <84928386+Hustoroff@users.noreply.github.com>
2021-11-11 20:23:03 +0300

    Saving user data to the database

--- piano-extension/scripts/btn-activate.js
+++ piano-extension/scripts/btn-activate.js
@@ -2,10 +2,10 @@
 // returns saved data
 const AuthStorageKey = "OAUTH2MPPE";
 
-async function updateUser(user_username, user_description) {
+async function updateUser(user_username, user_description, user_profileImage) {
     const token = (await chromeStorageGet(AuthStorageKey))[AuthStorageKey];
     if (!token) return;
-    mppews.send(JSON.stringify({ request: "update_user", payload: { userUsername: user_username, userDescription: user_description }, token: token }));
+    mppews.send(JSON.stringify({ request: "update_user", payload: { userUsername: user_username, userDescription: user_description, userprofileImage: user_profileImage }, token: token }));
 
 }
 
@@ -70,7 +70,8 @@ $("#infochange").on("click", function () {
     localStorage.setItem("save_changes_description", document.getElementById("description_input").value);
     var user_username = document.getElementById("nick_input").value;
     var user_description = document.getElementById("description_input").value;
-    updateUser(user_username, user_description);
+    var user_profileImage = document.getElementById("img_avatar").src;
+    updateUser(user_username, user_description, user_profileImage);
     $('#infochange').addClass("is-static");
 });
 
@@ -78,8 +79,14 @@ myfile.onchange = function () {
     if (this.files && this.files[0]) {
         var reader = new FileReader();
         reader.onload = function (e) {
+            localStorage.setItem("save_changes_nick", document.getElementById("nick_input").value);
+            localStorage.setItem("save_changes_description", document.getElementById("description_input").value);
             img_avatar.src = e.target.result;
+            var user_username = document.getElementById("nick_input").value;
+            var user_description = document.getElementById("description_input").value;
             localStorage.setItem("save_img_avatar", e.target.result);
+            user_profileImage = e.target.result;
+            updateUser(user_username, user_description, user_profileImage);
         };
         reader.readAsDataURL(this.files[0]);
     };
